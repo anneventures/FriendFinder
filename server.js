11b74311504c31dab2c057d72bae5c2935d0a90d
anneventures <info.annesimon@gmail.com>
2018-08-22 20:16:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES //
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// SET UP EXPRESS APP //
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// DATA PARSING //
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTER //
+require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// LISTENER //
+app.listen(PORT, function() {
+	console.log("App listening on port: " + PORT);
+});
