b1f7b94073662c8e11ad7f659c27ff5f60394753
anneventures <info.annesimon@gmail.com>
2018-08-24 18:11:21 -0400

    reduced amount of friends in list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 // DATA //
 var friendsArr = [
 	{
-		"name": "Curly Shirley",
-		"photo": "https://cdn.pixabay.com/photo/2015/09/02/12/51/woman-918707_640.jpg",
+		"name": "Shirley",
+		"photo": "http://www.yim778.com/data/out/38/759992.png",
 		"scores": [
 			5,
 			1,
@@ -17,24 +17,8 @@ var friendsArr = [
 		]
 	},
 	{
-		"name": "Techy Becky",
-		"photo": "https://cdn.pixabay.com/photo/2016/04/13/19/20/binary-1327493_640.jpg",
-		"scores": [
-			4,
-			5,
-			3,
-			4,
-			1,
-			2,
-			3,
-			4,
-			5,
-			4
-		]
-	},
-	{
-		"name": "Calm Tom",
-		"photo": "https://cdn.pixabay.com/photo/2018/03/13/15/01/male-3222718_640.jpg",
+		"name": "Zayne",
+		"photo": "https://img.clipartxtras.com/9fdf2f7d60a8905a35ba9d1b5336ff9c_male-cartoon-with-gotee-clip-art-at-clkercom-vector-clip-art-male-clipart-images_908-1380.svg",
 		"scores": [
 			3,
 			2,
@@ -46,22 +30,6 @@ var friendsArr = [
 			2,
 			1,
 			4,
-			1
-		]
-	},
-	{
-		"name": "Plain Zayn",
-		"photo": "https://cdn.pixabay.com/photo/2018/04/19/20/03/people-3334127_640.jpg",
-		"scores": [
-			4,
-			5,
-			5,
-			5,
-			4,
-			4,
-			5,
-			3,
-			2,
 			1
 		]
 	}
